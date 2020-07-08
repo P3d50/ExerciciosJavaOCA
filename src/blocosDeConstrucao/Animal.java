d8e9c81ac17f1258d5c6f67ab0544e08362861e8
noname <rpedromarcos@gmail.com>
2020-07-07 21:34:02 -0300

    atualizacao Animal() e MetodoMain()

--- src/blocosDeConstrucao/Animal.java
+++ src/blocosDeConstrucao/Animal.java
@@ -1,5 +1,33 @@
 package blocosDeConstrucao;
 
-public class EstruturaDeUmaClasse {
+/*
+ * Exemplo da estrutura de uma classe
+ */
+
+//classe com modificador de acesso "public" deve estar em um arquivo com o mesmo nome
+//deve haver apenas uma classe public para cada arquivo, posso ter várias classes e 
+//nenhuma ser public	
+public class Animal {
+	
+	//atributos e métodos são "membros da classe" - singleline
+	String nome;
+	
+	/** - javadoc
+	 *  Método para obter o nome do animal 
+	 *  
+	 * @return String - nome do animal
+	 * @author noname
+	 */
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String novoNome) {
+		/* multiline
+		 * recebe uma String e atribui ao nome do animal
+		 */
+		nome = novoNome;
+	}
+	
 
 }
--- src/blocosDeConstrucao/MetodoMain.java
+++ src/blocosDeConstrucao/MetodoMain.java
@@ -3,7 +3,7 @@ package blocosDeConstrucao;
 public class MetodoMain {
 	
 	//método main é o ponto inicial de execução da classe, 
-	//jvm procura um método com essa assinatura e executa
+	// a jvm procura um método com essa assinatura e executa
 	public static void main(String arg[]) {
 			
 	}
