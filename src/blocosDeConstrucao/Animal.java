ca202fcd286005201f087be7148ff396142db58a
noname <rpedromarcos@gmail.com>
2020-07-07 21:31:12 -0300

    adicionando exemplos do capítulo blocos de construção

---
+++ src/blocosDeConstrucao/Animal.java
@@ -0,0 +1,5 @@
+package blocosDeConstrucao;
+
+public class EstruturaDeUmaClasse {
+
+}
---
+++ src/blocosDeConstrucao/MetodoMain.java
@@ -0,0 +1,11 @@
+package blocosDeConstrucao;
+
+public class MetodoMain {
+	
+	//método main é o ponto inicial de execução da classe, 
+	//jvm procura um método com essa assinatura e executa
+	public static void main(String arg[]) {
+			
+	}
+
+}
